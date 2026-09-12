b7edf6a84a3e055187b1f7a95a0262331c694e1e
Sam Clemente <sam@theclementes.com>
2026-09-11 21:23:45 -0500

    fix: preserve metadata retry timing and lease ownership

--- services/wire-worker/Sources/WireWorkerCore/WireLinkMetadataTarget.swift
+++ services/wire-worker/Sources/WireWorkerCore/WireLinkMetadataTarget.swift
@@ -5,4 +5,16 @@ struct WireLinkMetadataTarget: Equatable, Sendable {
   let canonicalURL: String
   let etag: String?
   let lastModified: String?
+  let leaseExpiresAt: Date?
+
+  init(
+    canonicalKey: String, canonicalURL: String, etag: String?, lastModified: String?,
+    leaseExpiresAt: Date? = nil
+  ) {
+    self.canonicalKey = canonicalKey
+    self.canonicalURL = canonicalURL
+    self.etag = etag
+    self.lastModified = lastModified
+    self.leaseExpiresAt = leaseExpiresAt
+  }
 }
